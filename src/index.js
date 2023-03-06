96f2246ebec63c0cea1ecaf248469a46536e02f1
Julia <hercherjulia@gmail.com>
2023-03-06 21:59:39 +0100

    style changes

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ function showWeather(response) {
   currentTemperature.innerHTML = Math.round(response.data.main.temp);
   currentIcon.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   currentIcon.setAttribute("alt", response.data.weather[0].description);
   description.innerHTML = response.data.weather[0].description;
