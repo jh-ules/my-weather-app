739550f21bc0767a80f85d3f23716c8d8089ae62
Julia <hercherjulia@gmail.com>
2023-03-13 19:27:58 +0100

    created forecast variable and integrated API

--- src/index.js
+++ src/index.js
@@ -53,25 +53,40 @@ function formatDate(timestamp) {
   return `${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `   
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `   
       <div class="col-4">
          <div class="forecast">
-           <h6>${day}</h6>
+           <h6>${formatDay(forecastDay.dt)}</h6>
            <div class="weather-forecast-temperature">
-             <span class="forecast-temperature-max">11°</span>
-             <span class="forecast-temperature-min">4°</span>
+             <span class="forecast-temperature-max">${Math.round(
+               forecastDay.temp.max
+             )}°</span>
+             <span class="forecast-temperature-min">${Math.round(
+               forecastDay.temp.min
+             )}°</span>
            </div>
            <img
-             src="http://openweathermap.org/img/wn/01d@2x.png"
+             src="http://openweathermap.org/img/wn/${
+               forecastDay.weather[0].icon
+             }@2x.png"
              alt="weather icon"
              width="46"
              id="forecast-icon"
@@ -79,6 +94,7 @@ function displayForecast(response) {
          </div>
        </div>
      `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
